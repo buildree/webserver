bd7c6d6fe6fd92418b7cd7474950febbb845b3d9
hayashin10@gmail.com <hayashin10@gmail.com>
2025-04-10 21:37:32 +0900

    ファイルの整理

--- apache_pyenv.sh
+++ apache_pyenv.sh
@@ -4,16 +4,16 @@
 
 <<COMMENT
 作成者：サイトラボ
-URL：https://www.site-lab.jp/
 URL：https://buildree.com/
 
 注意点：conohaのポートは全て許可前提となります。もしくは80番、443番の許可をしておいてください。システムのfirewallはオン状態となります。centosユーザーのパスワードはランダム生成となります。最後に表示されます
 
 目的：システム更新+apache2.4系のインストール
-・apache2.4
+・apache2.4系
 ・mod_sslのインストール
 ・centosユーザーの作成
-・phpenvのインストール
+・pyenvのインストール
+・bottleとの連携
 
 COMMENT
 
@@ -30,16 +30,28 @@ echo "======================完了======================"
 echo ""
 }
 
-VAR='8.0.5'
-
-#Redhat系8か確認
+#CentOS7か確認
 if [ -e /etc/redhat-release ]; then
     DIST="redhat"
     DIST_VER=`cat /etc/redhat-release | sed -e "s/.*\s\([0-9]\)\..*/\1/"`
     #DIST_VER=`cat /etc/redhat-release | perl -pe 's/.*release ([0-9.]+) .*/$1/' | cut -d "." -f 1`
 
     if [ $DIST = "redhat" ];then
-      if [ $DIST_VER = "8" ];then
+      if [ $DIST_VER = "8" ] || [ $DIST_VER = "9" ];then
+
+      #SELinuxの確認
+SElinux=`which getenforce`
+if [ "`${SElinux}`" = "Disabled" ]; then
+  echo "SElinuxは無効なのでそのまま続けていきます"
+else
+  echo "SElinux有効のため、一時的に無効化します"
+  setenforce 0
+
+  getenforce
+  #exit 1
+fi
+
+
         #EPELリポジトリのインストール
         start_message
         dnf remove -y epel-release
@@ -48,35 +60,22 @@ if [ -e /etc/redhat-release ]; then
 
         #必要なパッケージのインストール
         start_message
+        dnf -y install git zlib-devel bzip2 bzip2-devel readline-devel sqlite sqlite-devel openssl-devel make gcc python3-devel libffi-devel
         end_message
 
 
-        start_message
+
+        # dnf updateを実行
         echo "dnf updateを実行します"
-        echo "dnf update"
-        #dnf -y update
+        echo ""
+
+        start_message
+        dnf -y update
         end_message
 
         # apacheのインストール
         echo "apacheをインストールします"
-        echo ""
-
-        dnf -y module install httpd
-
-        echo "ファイルのバックアップ"
-        echo ""
-        cp /etc/httpd/conf/httpd.conf /etc/httpd/conf/httpd.conf.bk
-
-        echo "htaccess有効化した状態のconfファイルを作成します"
-        echo ""
-
-        sed -i -e "151d" /etc/httpd/conf/httpd.conf
-        sed -i -e "151i AllowOverride All" /etc/httpd/conf/httpd.conf
-        sed -i -e "350i #バージョン非表示" /etc/httpd/conf/httpd.conf
-        sed -i -e "351i ServerTokens ProductOnly" /etc/httpd/conf/httpd.conf
-        sed -i -e "352i ServerSignature off \n" /etc/httpd/conf/httpd.conf
-
-
+        dnf  install -y httpd mod_ssl httpd-devel
 
         ls /etc/httpd/conf/
         echo "Apacheのバージョン確認"
@@ -95,78 +94,77 @@ SetEnvIfNoCase Request_URI\.(?:gif|jpe?g|png)$ no-gzip dont-vary
 Header append Vary User-Agent env=!dont-var
 EOF
 
+        #モジュールの読み込み
+        sed -i -e "353i LoadModule wsgi_module modules/mod_wsgi-py312.cpython-312-x86_64-linux-gnu.so \n" /etc/httpd/conf/httpd.conf
+        sed -i -e "354i WSGIScriptAlias / /var/www/html/adapter.wsgi \n" /etc/httpd/conf/httpd.conf
 
-        #phpのインストール
+
+        #pyenvの設定
+        echo "pythonのインストールをします"
+        curl -OL https://buildree.com/download/common/system/pyenv.sh
+        source ./pyenv.sh
+
+        #mod_wsgiのインストール
         start_message
-        echo "PHPをインストールします"
-        cat >/etc/profile.d/phpenv.sh <<'EOF'
-export PATH=/usr/local/bin/phpenv/bin:$PATH
-export PHPENV_ROOT=/usr/local/bin/phpenv
-EOF
-
-        source /etc/profile.d/phpenv.sh
+        echo "mod_wsgiのインストール"
+        pip install mod-wsgi
         end_message
 
-        #phpenvの取得
+        #インストール場所を調べる
         start_message
-        echo "phpenvをクーロンします"
-        echo "/usr/local/bin/phpenv にインストールします"
-        echo "curl -L https://raw.githubusercontent.com/phpenv/phpenv-installer/master/bin/phpenv-installer"
-        curl -L https://raw.githubusercontent.com/phpenv/phpenv-installer/master/bin/phpenv-installer \
-            | PHPENV_ROOT=/usr/local/bin/phpenv bash
+        echo "インストール場所を調べます"
+        pip show mod-wsgi
+        ls -all /usr/local/pyenv/versions/3.12.5/lib/python3.12/site-packages/mod_wsgi/server/
         end_message
 
-        #環境変数を通す
+        #ファイルのコピー
         start_message
-        echo "環境変数を通す"
-        echo 'eval "$(phpenv init -)"' >> /etc/profile.d/phpenv.sh
-        echo "ソース環境を反映"
-        echo "source /etc/profile.d/phpenv.sh"
-        source /etc/profile.d/phpenv.sh
+        echo "ファイルをコピーします"
+        cp  /usr/local/pyenv/versions/3.12.5/lib/python3.12/site-packages/mod_wsgi/server/mod_wsgi-py312.cpython-312-x86_64-linux-gnu.so /etc/httpd/modules/
+        echo "ファイルの確認"
+        ls /etc/httpd/modules/
         end_message
 
-        #Apacheと連携できるように設定
+        #botleのインストール
         start_message
-        echo "Apacheと連携できるようにします"
-        sed -i -e '1i configure_option "--with-apxs2" "/usr/bin/apxs"' /usr/local/bin/phpenv/plugins/php-build/share/php-build/definitions/8.0.5
-        echo "設定確認"
-        cat /usr/local/bin/phpenv/plugins/php-build/share/php-build/definitions/8.0.5
+        echo "botleのインストール"
+        pip install bottle
+        cp /usr/local/pyenv/versions/3.12.5/lib/python3.12/site-packages/bottle.py /var/www/html/
         end_message
 
-
-        #phpの確認とインストール
+        #wsgiファイル
         start_message
-        echo "phpenvのインストール phpenv install -l"
-        phpenv install -l
-        echo "php8.0.5のインストール"
-        phpenv install 8.0.5
-        echo $VAR
-        phpenv global 8.0.5
-        end_message
-
-
-        #apacheと連携
-        start_message
-        cat >/etc/httpd/conf.d/php.conf <<'EOF'
-LoadModule php_module /etc/httpd/modules/libphp.so
-
-AddType application/x-httpd-php .php
-DirectoryIndex index.php
+        cat >/var/www/html/adapter.wsgi <<'EOF'
+import sys, os
+dirpath = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(dirpath)
+os.chdir(dirpath)
+import bottle
+import index
+application = bottle.default_app()
 EOF
         end_message
 
-        # phpinfoの作成
+        #pythonファイル
         start_message
-        touch /var/www/html/info.php
-        echo '<?php phpinfo(); ?>' >> /var/www/html/info.php
-        cat /var/www/html/info.php
+        cat >/var/www/html/index.py <<'EOF'
+from bottle import route, run, template
+from bottle import TEMPLATE_PATH
+
+@route('/')
+def index():
+    return ("BuildreeによりApacheとbottleの連携！")
+
+if __name__ == '__main__':
+    run(host='0.0.0.0', port=8081, debug=True, reloader=True)
+EOF
         end_message
 
 
         #ユーザー作成
         start_message
-        echo "centosユーザーを作成します"
-        USERNAME='centos'
+        echo "ユーザーを作成します"
+        USERNAME='user'
         PASSWORD=$(more /dev/urandom  | tr -d -c '[:alnum:]' | fold -w 10 | head -1)
 
         useradd -m -G apache -s /bin/bash "${USERNAME}"
@@ -176,12 +174,6 @@ EOF
         #所属グループ表示
         echo "所属グループを表示します"
         getent group apache
-        end_message
-
-        #所有者の変更
-        start_message
-        echo "ドキュメントルートの所有者をcentos、グループをapacheにします"
-        chown -R centos:apache /var/www/html
         end_message
 
         # apacheの起動
@@ -219,14 +211,9 @@ EOF
         umask 0002
 
         cat <<EOF
-        http://IPアドレス/info.php
-        https://IPアドレス/info.php
+        http://IPアドレス
+        https://IPアドレス
         で確認してみてください
-
-        PHP8.0.5がインストールされております
-        php.iniは
-        /usr/local/phpenv/versions/7.4.13/etc/php.ini
-        です
 
         ドキュメントルート(DR)は
         /var/www/html
